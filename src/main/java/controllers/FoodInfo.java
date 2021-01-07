8deb3824ed109a2689efea5e25714b358a07a2ed
CharlieHarris898 <92456@farnborough.ac.uk>
2021-01-07 00:49:21 +0000

    Exercises and Recipes page done (just food info now)

--- src/main/java/controllers/FoodInfo.java
+++ src/main/java/controllers/FoodInfo.java
@@ -1,48 +1,41 @@
 package controllers;
 
+import org.glassfish.jersey.media.multipart.FormDataParam;
 import server.Main;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.Consumes;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Cookie;
 import javax.ws.rs.core.MediaType;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONArray;
 
+import static server.Convertor.convertToJSONObject;
 
 
-@Path("FoodInfo")
+@Path("foodinfo/")
 @Consumes(MediaType.MULTIPART_FORM_DATA)
 @Produces(MediaType.APPLICATION_JSON)
 
 public class FoodInfo {
-    @GET
+    @POST
     @Path("list")
-    public String FoodInfoList() {
-        System.out.println("Invoked FoodInfo.FoodInfoList()");
-        JSONArray response = new JSONArray();
+    public String getFoodInfo(@FormDataParam("Food") String name) {
+
+        System.out.println("Invoked FoodInfo.getFood()");
         try {
-            PreparedStatement ps = Main.db.prepareStatement("SELECT FoodID, Vegetarian, Vegan, Carbohydrate, FruitAndVeg, Dairy, Protein, Fat FROM FoodInfo");
-            ResultSet results = ps.executeQuery();
-            while (results.next() == true) {
-                JSONObject row = new JSONObject();
-                row.put("FoodID", results.getInt(1));
-                row.put("Vegetarian", results.getBoolean(2));
-                row.put("Vegan", results.getBoolean(3));
-                row.put("Carbohydrate", results.getBoolean(4));
-                row.put("FruitAndVeg", results.getBoolean(5));
-                row.put("Dairy", results.getBoolean(6));
-                row.put("Protein", results.getBoolean(7));
-                row.put("Fat", results.getBoolean(8));
-                response.add(row);
-            }
-            return response.toString();
-        } catch (Exception exception) {
-            System.out.println("Database error: " + exception.getMessage());
-            return "{\"Error\": \"Unable to list items.  Error code xx.\"}";
+            PreparedStatement statement = Main.db.prepareStatement(
+                    "SELECT * FROM Foods WHERE Name = ?"
+            );
+            statement.setString(1, name);
+            ResultSet resultSet = statement.executeQuery();
+            JSONObject resultsJSON = convertToJSONObject(resultSet);
+            return resultsJSON.toString();
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return "{\"Error\": \"Something as gone wrong.  Please contact the administrator with the error code UC-UG. \"}";
         }
     }
 }
